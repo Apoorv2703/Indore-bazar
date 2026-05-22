a2b5a48118f6b293052559b0073a5d7b90a0f7f9
Apoorv - Patel <apoorvpatel3277@gmail.com>
2026-05-22 22:28:22 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
-import {connectDB} from "./config/dbconfig.js"
+import {connectDB} from "./config/dbConfig.js"
 dotenv.config()
 
 //local imports
